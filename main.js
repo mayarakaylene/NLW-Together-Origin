06462e942d99a0015fd673ce3f125076ae68b428
mayarakaylene <may.kaylene@gmail.com>
2022-05-13 03:57:17 -0300

    Aula 03

--- main.js
+++ main.js
@@ -28,3 +28,32 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials carousel slider slider swiper */
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .image,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonmials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)

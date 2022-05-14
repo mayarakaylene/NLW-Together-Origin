aecc9f6ffd49e4266297dfe05b6dadd6eed0c1d8
mayarakaylene <may.kaylene@gmail.com>
2022-05-13 23:26:40 -0300

    Aula 04

--- main.js
+++ main.js
@@ -18,19 +18,18 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll*/
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials carousel slider slider swiper */
-
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   pagination: {
@@ -53,7 +52,25 @@ scrollReveal.reveal(
   #about .image, #about .image,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonmials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+/* Botão voltar para o topo */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
